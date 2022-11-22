8b0830876db21f9ea0384f75260b88ffa88f9d52
Anton Aliokhna <antonaliokhna@gmail.com>
2022-11-23 00:09:32 +0300

    Add service unit tests

--- VerificationMailAppTests/Service/NetworkDataFetcherTests.swift
+++ VerificationMailAppTests/Service/NetworkDataFetcherTests.swift
@@ -6,30 +6,38 @@
 //
 
 import XCTest
+@testable import VerificationMailApp
 
 final class NetworkDataFetcherTests: XCTestCase {
 
+    var sut: NetworkDataFetcher!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = NetworkDataFetcher()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
+    
+    func testParsingJSONDataFromApi() {
+        sut.networkService = MockCorrectMailReponseModelNetworkServise()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        sut.fetchGenericJSONData(stringUrl: "") { (mailResponseModel: MailResponseModel?) in
+            XCTAssertNotNil(mailResponseModel, "fetchGenericJSONData returned nil")
         }
     }
 
+    final class MockCorrectMailReponseModelNetworkServise: Networkign {
+        func request(stringUrl: String, completion: @escaping (Data?, Error?) -> Void) {
+            let mailResponseModel = MailResponseModel(
+                email: "antonaliokhna@gmail.com",
+                autocorrect: "",
+                deliverability: "deliverable"
+            )
+
+            let data = try? JSONEncoder().encode(mailResponseModel)
+            completion(data, nil)
+        }
+    }
 }
